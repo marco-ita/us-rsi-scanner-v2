f1c9908f486076fd276a21396a267640b1933066
Marco <marcodecesaris.it@gmail.com>
2026-07-30 19:46:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import os
 
+
 st.set_page_config(page_title="US Market Scanner - Ultra Fast", layout="wide")
 
 st.title("📈 Scanner USA - RSI & Trend Calibrato (SMA 40 / 200)")
